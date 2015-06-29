7cbe0fb4c4cac5103b07014d1ba9c15bcde506c1
nmmichalak <michalak.nicholas@gmail.com>
2015-06-29 15:04:53 -0500

    added more code
    more plots and lm and anova code

--- making fake data for GitHub.R
+++ making fake data for GitHub.R
@@ -8,3 +8,24 @@ write.table(fakedata,"fakedata.csv",sep=",",row.names = FALSE,col.names = TRUE)
 fakedata.url <- url("https://raw.githubusercontent.com/nmmichalak/test-repo/master/fakedata.csv")
 fakedata <- read.csv(fakedata.url,sep=",",header = TRUE)
 head(fakedata)
+library(ggplot2)
+library(Hmisc)
+densities <- sapply(list("dv1","dv2"),function(i) {
+  ggplot(data=fakedata,aes_string(x=i,fill="condition")) +
+    geom_density(aes_string(x=i,fill="condition"),alpha=.2) +
+    labs(x=paste(toupper(i)),color="Condition",title="Distributions by Condition")
+},
+simplify = FALSE,USE.NAMES = TRUE)
+plots <- sapply(list("dv1","dv2"),function(i) {
+  ggplot(data=fakedata,aes_string(x="condition",y=i,fill="condition")) +
+  stat_summary(fun.data="mean_cl_boot",aes_string(x="condition",y=i),geom="bar") +
+  stat_summary(fun.data="mean_cl_boot",aes_string(x="condition",y=i),geom="errorbar",width=.2) +
+  labs(x=NULL,y=paste(toupper(i)),fill="Condition",title="95% CIs")
+},
+simplify = FALSE,USE.NAMES = TRUE)
+anovas <- sapply(list("dv1","dv2"),function(i) {
+  formula <- paste0(i,"~condition")
+  summary(aov(lm(formula,fakedata)))
+},
+simplify = FALSE,USE.NAMES = TRUE)
+densities
